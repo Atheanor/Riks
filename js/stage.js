b6d09e7b6caaa306434a503b2df862ebbf7b3dc1
Atheanor <germain.chapot@gmail.com>
2013-04-03 17:47:34 +0100

    Fix issue with paralaxe

--- js/stage.js
+++ js/stage.js
@@ -51,44 +51,49 @@ Stage.prototype.display = function() {
 	this.background.style.top = "0px";
 	this.background.style.left = (-(window.innerWidth/2)).toString() + "px";
 	this.background.style.zIndex = "-2";
-	
+
 	this.foreground.width = window.innerWidth*2;
 	this.foreground.height = window.innerHeight/6;
 	this.foreground.style.position = "absolute";
 	this.foreground.style.top = (window.innerHeight*5/6).toString() + "px";
 	this.foreground.style.left = (-(window.innerWidth/2)).toString() + "px";
 	this.foreground.style.zIndex = "-1";
-	
+
 	document.body.appendChild(this.background);
 	document.body.appendChild(this.foreground);
 };
 
-Stage.prototype.update = function(charaOne, charaTwo) {
+Stage.prototype.update = function(charaLeft, charaRight) {
 	// We assure that the first character is the left one
-	if(charaOne.positionX > charaTwo.positionX) {
+	if(charaLeft.positionX > charaRight.positionX) {
 		// If not
-		this.update(charaTwo,charaOne);
+		this.update(charaRight,charaLeft);
 	} else {
+
 		var margin = 60, // Margin needed between a character and the screen
 			screenWidth = window.innerWidth, // Width of the canvas displayed
 			foregroundPosX = this.foreground.offsetLeft, 
 			backgroundPosX = this.background.offsetLeft;
 			
-		if(charaOne.positionX <= margin && // The left character is moving close to the left border of the screen
-		charaTwo.positionX + charaTwo.width + margin + 10 < screenWidth && // Checking if the right one isn't blocking the other side
+		if(charaLeft.positionX <= margin && // The left character is moving close to the left border of the screen
+		charaRight.positionX + charaRight.width + margin + 10 < screenWidth && // Checking if the right one isn't blocking the other side
 		foregroundPosX < -(margin)) { // The left border of the foreground has to be at a little distance from the left border of the canvas
 			//Scroll Left
 			foregroundPosX += 30;
 			backgroundPosX += 6;
-			charaTwo.positionX += 30;
+			charaRight.positionX += 30;
+			charaLeft.positionX += 30;
+	
 		}
-		else if(charaTwo.positionX + charaTwo.width + margin >= screenWidth && // The right character is moving close to the right border
-		charaOne.positionX > margin + 10 && // While the left character is not blocking the other border
+		else if(charaRight.positionX + charaRight.width + margin >= screenWidth && // The right character is moving close to the right border
+		charaLeft.positionX > margin + 10 && // While the left character is not blocking the other border
 		foregroundPosX - margin > -(screenWidth)) { // The right of the foreground has to be a margin away from the right of the canvas
 			//Scroll Right
 			foregroundPosX -= 30;
 			backgroundPosX -= 6;
-			charaOne.positionX -= 30;
+			charaLeft.positionX -= 30;
+			charaRight.positionX -= 30;
+			
 		}
 		this.foreground.style.left = (foregroundPosX).toString() + "px";
 		this.background.style.left = (backgroundPosX).toString() + "px";
